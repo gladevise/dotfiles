ea2d14bdb2259c3d69f30fc2d208cda51f9d366f
gladevise <gladevise@gmail.com>
2021-05-04 17:38:38 +0900

    feat: Update nvm

--- install_node.sh
+++ install_node.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # install nvm & Node.js & npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm install lts/*
+nvm install lts/*  --reinstall-packages-from=node
 nvm use --lts
 cat ./npm_install_list.txt  | xargs npm -g install

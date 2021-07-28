7a1133216b9eca9a62ac87a13da245aa7b27740c
gladevise <gladevise@gmail.com>
2021-07-28 09:14:31 +0900

    fix: Cannot find nvm command

--- scripts/install_npm.sh
+++ scripts/install_npm.sh
@@ -4,6 +4,10 @@ pushd $PWD
 cd `dirname $0`
 echo $PWD
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 nvm use --lts
 cat ./npm_install_list.txt  | xargs npm -g install
 

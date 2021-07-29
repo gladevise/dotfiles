113a494bad87eed5bfcd66b16b38990c90786a1e
gladevise <gladevise@gmail.com>
2021-07-29 14:48:37 +0900

    fix: Set nvm paths to upgrade_npm script

--- scripts/upgrade_npm.sh
+++ scripts/upgrade_npm.sh
@@ -9,6 +9,9 @@ ncu -g | capture_command
 upgradable_packages=$(echo $(get_command) | sed -En 's/.*npm -g install (.*)$/\1/gp')
 
 if [[ -n $upgradable_packages ]];then
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
   nvm use --lts
   npm -g install $upgradable_packages
 fi

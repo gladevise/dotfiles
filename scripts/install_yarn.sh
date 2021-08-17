2109c61f47c26d485276549c2786f9278aa47459
gladevise <gladevise@gmail.com>
2021-08-17 21:49:42 +0900

    feat: set -y option to apt install

--- scripts/install_yarn.sh
+++ scripts/install_yarn.sh
@@ -1,5 +1,5 @@
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update
-sudo apt install --no-install-recommends yarn
+sudo apt install -y --no-install-recommends yarn
 yarn --version

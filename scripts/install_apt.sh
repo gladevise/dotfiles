b399c2e912758e8af781135b3e7d8b9b34b98a56
gladevise <gladevise@gmail.com>
2021-09-07 21:59:56 +0900

    feat: Add kdenlive to apt list

--- scripts/install_apt.sh
+++ scripts/install_apt.sh
@@ -8,30 +8,37 @@ echo $PWD
 # ppaを追加する
 ## Add copyq ppa
 if [ -f "/etc/apt/sources.list.d/hluk-ubuntu-copyq-focal.list" ]; then
-  echo "ppa is already existed"
+  echo "copyq ppa is already existed"
 else
   sudo add-apt-repository -y ppa:hluk/copyq
 fi
 
 ## Add lazygit ppa
 if [ -f "/etc/apt/sources.list.d/lazygit-team-ubuntu-release-focal.list" ]; then
-  echo "ppa is already existed"
+  echo "lazygit ppa is already existed"
 else
   sudo add-apt-repository -y ppa:lazygit-team/release
 fi
 
 ## Add obs ppa
 if [ -f "/etc/apt/sources.list.d/obsproject-ubuntu-obs-studio-focal.list" ]; then
-  echo "ppa is already existed"
+  echo "obs ppa is already existed"
 else
   sudo add-apt-repository -y ppa:obsproject/obs-studio
 fi
 
 ## Add git ppa
 if [ -f "/etc/apt/sources.list.d/git-core-ubuntu-ppa-focal.list" ]; then
-  echo "ppa is already existed"
+  echo "git ppa is already existed"
 else
   sudo add-apt-repository -y ppa:git-core/ppa
+fi
+
+## Add kdenlive ppa
+if [ -f "/etc/apt/sources.list.d/kdenlive-ubuntu-kdenlive-stable-focal.list" ]; then
+  echo "kdenlive ppa is already existed"
+else
+  sudo add-apt-repository -y ppa:kdenlive/kdenlive-stable
 fi
 
 # update & upgrade

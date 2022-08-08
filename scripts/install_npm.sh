16fb8757ae572affa0ee0ae3c636c4e95f87f011
gladevise <gladevise@gmail.com>
2022-08-08 18:18:43 +0900

    refactor: Use bash array as package list

--- scripts/install_npm.sh
+++ scripts/install_npm.sh
@@ -4,11 +4,28 @@ pushd $PWD
 cd `dirname $0`
 echo $PWD
 
+
+declare -a packages
+
+packages=(
+  # Linter
+  eslint
+
+  # CLI Tools
+  serve
+  nodemon
+  lighthouse
+  @jarred/git-peek
+
+  # Package updater
+  npm-check-updates
+)
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 nvm use --lts
-cat ./npm_install_list.txt  | xargs npm -g install
+npm -g install ${packages[@]}
 
 popd
--- scripts/install_snap.sh
+++ scripts/install_snap.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-# Install snap packages
 pushd $PWD
 cd `dirname $0`
 echo $PWD
 
-cat ./snap_install_list.txt  | xargs sudo snap install
+declare -a packages
+
+packages=(
+)
+
+sudo snap install ${packages[@]}
 
 popd

2017275d450c5b19fc98b3934ad20b2f8aade6ff
gladevise <gladevise@gmail.com>
2022-09-06 09:53:26 +0900

    chore: Add NeoVim requirements

--- scripts/install_nvim.sh
+++ scripts/install_nvim.sh
@@ -13,7 +13,15 @@ else
   curl -L -o ~/dotfiles/.cache/nvim --create-dirs https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
   chmod 755 ~/dotfiles/.cache/nvim
   sudo cp ~/dotfiles/.cache/nvim /usr/local/bin/nvim
+
+  # Install neovim python
   pip3 install --user pynvim
+
+  # load nvm
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+  nvm use --lts
   npm install -g neovim
 fi
 

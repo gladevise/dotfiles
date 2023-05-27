23b40f728bd9045de4c49fb66ca7898d70eebe5e
gladevise <gladevise@gmail.com>
2023-05-27 11:07:41 +0900

    chore: Add script to install blender

--- scripts/install_snap.sh
+++ scripts/install_snap.sh
@@ -3,11 +3,13 @@ pushd $PWD
 cd `dirname $0`
 echo $PWD
 
-declare -a packages
+# declare -a packages
 
-packages=(
-)
+# packages=(
+# )
 
-sudo snap install ${packages[@]}
+# sudo snap install ${packages[@]}
+
+sudo snap install blender --classic
 
 popd

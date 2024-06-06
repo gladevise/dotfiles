e8e5e7497c919b2614e38150caf71da884b210e5
gladevise <gladevise@gmail.com>
2024-06-06 09:30:28 +0900

    chore: Update installation script for OpenShot.AppImage
    Remove commented out code and unnecessary echo statements from the installation script for OpenShot.AppImage.

--- scripts/install_appimages.sh
+++ scripts/install_appimages.sh
@@ -4,15 +4,15 @@ pushd $PWD
 cd `dirname $0`
 echo $PWD
 
-if [[ $(command -v OpenShot.AppImage) ]]; then
-  echo "OpenShot is already installed"
-else
-  echo "OpenShot isn't installed"
-  curl -L -o ~/dotfiles/.cache/OpenShot.AppImage --create-dirs https://github.com/OpenShot/openshot-qt/releases/download/v3.0.0/OpenShot-v3.0.0-x86_64.AppImage
-  chmod 755 ~/dotfiles/.cache/OpenShot.AppImage
-  cp ~/dotfiles/.cache/OpenShot.AppImage ~/.local/bin/OpenShot.AppImage
-fi
+# if [[ $(command -v OpenShot.AppImage) ]]; then
+#   echo "OpenShot is already installed"
+# else
+#   echo "OpenShot isn't installed"
+#   curl -L -o ~/dotfiles/.cache/OpenShot.AppImage --create-dirs https://github.com/OpenShot/openshot-qt/releases/download/v3.0.0/OpenShot-v3.0.0-x86_64.AppImage
+#   chmod 755 ~/dotfiles/.cache/OpenShot.AppImage
+#   cp ~/dotfiles/.cache/OpenShot.AppImage ~/.local/bin/OpenShot.AppImage
+# fi
 
-OpenShot.AppImage --version
+# OpenShot.AppImage --version
 
 popd

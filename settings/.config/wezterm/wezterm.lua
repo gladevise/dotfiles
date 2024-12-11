d4efd07e656fb1595c10924598c6bd3f3898054f
gladevise <77527767+gladevise@users.noreply.github.com>
2024-12-11 16:02:26 +0900

    feat: Update WezTerm keybindings for improved pane management and adjust leader key timeout

--- settings/.config/wezterm/wezterm.lua
+++ settings/.config/wezterm/wezterm.lua
@@ -15,14 +15,14 @@ end)
 
 -- keybindings
 local act = require "wezterm".action
-config.leader = { key = 'Space', mods = 'CTRL', timeout_milliseconds = 1000 }
+config.leader = { key = 'Space', mods = 'CTRL', timeout_milliseconds = 1500 }
 config.keys = {
   -- Send leader
-  {
-    key = 'LEADER|SHIFT',
-    mods = 'Space',
-    action = wezterm.action.SendKey { key = 'Space', mods = 'CTRL' },
-  },
+  -- {
+  --   key = 'LEADER|SHIFT',
+  --   mods = 'Space',
+  --   action = wezterm.action.SendKey { key = 'Space', mods = 'CTRL' },
+  -- },
 
   -- Open workspace launcher
   {
@@ -77,9 +77,44 @@ config.keys = {
   -- This will create a new split and run your default program inside it
   {
     mods = 'LEADER',
-    key = 'e',
+    key = 'i',
     action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
   },
+  {
+    mods = 'LEADER',
+    key = '-',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+  -- kill pane
+  {
+    mods = 'LEADER',
+    key = 'x',
+    action = wezterm.action.CloseCurrentPane { confirm = true },
+  },
+  -- move pane focus
+  {
+    mods = 'LEADER',
+    key = 'h',
+    action = wezterm.action.ActivatePaneDirection "Left",
+  },
+  {
+    mods = 'LEADER',
+    key = 'j',
+    action = wezterm.action.ActivatePaneDirection "Down",
+  },
+  {
+    mods = 'LEADER',
+    key = 'k',
+    action = wezterm.action.ActivatePaneDirection "Up",
+  },
+  {
+    mods = 'LEADER',
+    key = 'l',
+    action = wezterm.action.ActivatePaneDirection "Right",
+  },
+  -- Move Tabs
+  { key = 'h', mods = 'CTRL', action = act.ActivateTabRelative(-1) },
+  { key = 'l', mods = 'CTRL', action = act.ActivateTabRelative(1) },
 }
 
 -- and finally, return the configuration to wezterm

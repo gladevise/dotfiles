23aa8c01a20c87aff93523064fe894eeffe2054d
gladevise <77527767+gladevise@users.noreply.github.com>
2025-02-17 08:58:04 +0900

    feat: Set Nautilus default sort order to 'mtime' and enable reverse sorting

--- scripts/gsettings.sh
+++ scripts/gsettings.sh
@@ -5,4 +5,8 @@ echo $PWD
 
 gsettings set org.freedesktop.ibus.panel.emoji hotkey "['<Super>period']"
 
+# set nautilus default sort order
+gsettings set org.gnome.nautilus.preferences default-sort-order 'mtime'
+gsettings set org.gnome.nautilus.preferences default-sort-in-reverse-order 'true'
+
 popd
